5ff2cd4e90163f266d476c266e6d7527bcc70e7b
Nikyshah07 <nikyshah07@gmail.com>
2025-02-25 20:30:22 +0530

    storage changess

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ const { server,app } = require("./Socket/socket.js");
 
 dotenv.config()
 // const app=express();
-// app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your frontend URL
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", true); // Allow cookies
-    next();
-});
-app.use(cors({
-    origin: 'http://localhost:3000', // Your frontend URL
-    credentials: true // Allow credentials (cookies)
-}));
+app.use(cors());
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your frontend URL
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     res.header("Access-Control-Allow-Credentials", true); // Allow cookies
+//     next();
+// });
+// app.use(cors({
+//     origin: 'https://chatappbackend-x8om.onrender.com', // Your frontend URL
+//     credentials: true // Allow credentials (cookies)
+// }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
